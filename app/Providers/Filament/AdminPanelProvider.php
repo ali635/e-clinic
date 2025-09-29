52f50d8b1d51d38228b218b0ecdcfaa87871b2d9
mohamed ali <mohamedali7659@gmail.com>
2025-09-29 23:26:36 +0300

    `Added Filament Awin Theme package and integrated it into the admin panel`

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -20,6 +20,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Resma\FilamentAwinTheme\FilamentAwinTheme;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -45,6 +46,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->plugins([
                 ModulesPlugin::make(),
+                FilamentAwinTheme::make(),
             ])
             ->topNavigation(false)
             ->middleware([
