490721b03eb100b38dbab45ef70ecebdc34900e1
mohamed ali <mohamedali7659@gmail.com>
2025-10-07 23:20:00 +0300

    `Added FeedbackController and API routes for patient feedback`

--- Modules/Patient/routes/api.php
+++ Modules/Patient/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Patient\Http\Controllers\API\AuthController;
 use Modules\Patient\Http\Controllers\API\DiseaseController;
+use Modules\Patient\Http\Controllers\API\FeedbackController;
 use Modules\Patient\Http\Controllers\PatientController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
@@ -16,5 +17,11 @@ Route::prefix('v1')->group(function () {
     });
     Route::prefix('diseases')->group(function () {
         Route::get('/', [DiseaseController::class, 'index']);
+    });
+    Route::middleware(['auth:api'])->group(function () {
+        Route::prefix('patient/feedback')->group(function () {
+            Route::get('/', [FeedbackController::class, 'index']);
+            Route::post('store', [FeedbackController::class, 'store']);
+        });
     });
 });
