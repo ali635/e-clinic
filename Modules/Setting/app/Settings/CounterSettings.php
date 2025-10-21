a7a131940ecb25c35d9ec2810ffc527abcd0749f
mohamed ali <mohamedali7659@gmail.com>
2025-10-21 17:44:45 +0300

    `Moved CounterSettings from Slider module to Setting module and updated related code`

--- Modules/Setting/app/Settings/CounterSettings.php
+++ Modules/Setting/app/Settings/CounterSettings.php
@@ -1,12 +1,16 @@
 <?php
 
-namespace Modules\Slider\Settings;
-
+namespace Modules\Setting\Settings;
 
 use Spatie\LaravelSettings\Settings;
+use Spatie\Translatable\HasTranslations;
 
 class CounterSettings extends Settings
 {
+    use HasTranslations;
+
+    protected array $translatable = ['counter_settings'];
+
     public ?array $counter_settings = [];
 
     public static function group(): string
@@ -14,4 +18,3 @@ class CounterSettings extends Settings
         return 'counter';
     }
 }
-
