f34b2d6ea31869de582995faa86a4cacf2815b1e
mohamed ali <mohamedali7659@gmail.com>
2025-11-01 21:21:38 +0200

    `Refactor PatientResource to use whenLoaded and map methods for eager loading and transforming related data`

--- Modules/Patient/app/Http/Resources/PatientResource.php
+++ Modules/Patient/app/Http/Resources/PatientResource.php
@@ -20,28 +20,44 @@ class PatientResource extends JsonResource
             'status'         => $this->status,
             'other_phone'    => $this->other_phone,
             'hear_about_us'  => $this->hear_about_us,
-            'country' => $this->country ? [
-                'id' => $this->country->id,
-                'name' => $this->country->getTranslation('name', app()->getLocale())->name,
-            ] : null,
 
-            'city' => $this->city ? [
-                'id' => $this->city->id,
-                'name' => $this->city->getTranslation('name', app()->getLocale())->name,
-            ] : null,
-
-
-            'area' => $this->area ? [
-                'id' => $this->area->id,
-                'name' => $this->area->getTranslation('name', app()->getLocale())->name,
-            ] : null,
-
-            'diseases' => $this->diseasesMany->map(function ($disease) {
+            'country' => $this->whenLoaded('country', function () {
                 return [
-                    'id' => $disease->id,
-                    'name' => $disease->getTranslation('name', app()->getLocale())->name,
+                    'id' => $this->country->id,
+                    'name' => $this->country->name ?? '',
+                    'status' => $this->country->status,
+                    'order' => $this->country->order,
                 ];
             }),
+
+            'city' => $this->whenLoaded('city', function () {
+                return [
+                    'id' => $this->city->id,
+                    'name' => $this->city->name ?? '',
+                    'status' => $this->city->status,
+                    'order' => $this->city->order,
+
+                ];
+            }),
+
+            'area' => $this->whenLoaded('area', function () {
+                return [
+                    'id' => $this->area->id,
+                    'name' => $this->area->name ?? '',
+                    'status' => $this->area->status,
+                    'order' => $this->area->order,
+                ];
+            }),
+
+            'diseases' => $this->whenLoaded('diseasesMany', function () {
+                return $this->diseasesMany->map(function ($disease) {
+                    return [
+                        'id' => $disease->id,
+                        'name' => $disease->getTranslation('name', app()->getLocale())->name,
+                    ];
+                });
+            })
+
         ];
     }
 }
