02abe0bafa704dec3398c0f98a3b991723745e43
mohamed ali <mohamedali7659@gmail.com>
2026-01-10 14:16:23 +0200

    done

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -190,8 +190,8 @@
             display: flex;
             align-items: flex-start;
             margin-bottom: .5rem;
-            font-weight: 700;
-            font-size: 15px;
+            font-weight: 400;
+            font-size: 10px;
             line-height: 1;
         }
 
